591f43786f0a690fc1a1f756a2832ce19337b40a
Ivar <ivar.grovik@gmail.com>
2022-06-10 17:26:32 +0200

    test2

--- read_infineon.py
+++ read_infineon.py
@@ -1,9 +1,9 @@
-import smbus
+import smbus2
 
 # I2C channel 1 is connected to the GPIO pins
 channel = 1
 
-#  MCP4725 defaults to address 0x60
+
 address = 0x76
 temp_address = [0x03, 0x04, 0x05]
 press_address = [0x00, 0x01, 0x02]
@@ -12,11 +12,10 @@ press_config = 0x06
 temp_config = 0x07
 meas_config = 0x08
 
-# Register addresses (with "normal mode" power-down bits)
-reg_write_dac = 0x40
+
 
 # Initialize I2C (SMBus)
-bus = smbus.SMBus(channel)
+bus = smbus2.SMBus(channel)
 
 # Create a sawtooth wave 16 times
 cont = True
