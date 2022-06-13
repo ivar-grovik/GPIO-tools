700657f59c90b3991734b0f215bdf801b13a2db1
Ivar <ivar.grovik@gmail.com>
2022-06-13 21:17:25 +0200

    added ByteTools.py

--- read_infineon.py
+++ read_infineon.py
@@ -1,8 +1,9 @@
 import smbus2
 
 # I2C channel 1 is connected to the GPIO pins
-channel = 1
+import ByteTools
 
+channel = 1
 
 address = 0x76
 temp_address = [0x03, 0x04, 0x05]
@@ -11,31 +12,16 @@ ID_address = 0x0D
 press_config = 0x06
 temp_config = 0x07
 
-
-
 # Initialize I2C (SMBus)
 bus = smbus2.SMBus(channel)
 
-# Create a sawtooth wave 16 times
-cont = True
-
-test = bus.read_byte_data(address,ID_address)
-print(hex(test))
-#Read temperature
-#for i in range(0, 3):
-#    print(temp_address[i])
-#    print(bus.read_i2c_block_data(address,temp_address[i], 32))
-#    print(bus.read_byte_data(address,temp_address[i]))
-#    print("\n")
-
-#print(hex(bus.read_i2c_block_data(address,0x03, 3)))
-
+# Init methods
+# bus.write_byte_data(address, 0x08, 0x02)
 bus.write_byte_data(address, 0x08, 0x06)
 
-for i in range(0,3):
-    print(hex(bus.read_byte_data(address, temp_address[i])))
+bits = ["", "", ""]
+for i in range(0, 3):
+    bit = bus.read_byte_data(address, temp_address[i])
+    bits[i] = bin(bit)
 
-print(hex(bus.read_byte_data(address, 0x08)))
-
-#bus.write_byte_data(address, 0x08, 0x02)
-
+print(ByteTools.bin2int(bits, 8))
