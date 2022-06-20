ca9916de238c438aa540aca9391aaac7cc4310bf
Ivar <ivar.grovik@gmail.com>
2022-06-20 20:08:04 +0200

    bugfixing lack of state reset

--- test_Sensors.py
+++ test_Sensors.py
@@ -7,7 +7,7 @@ class InfineonTests(unittest.TestCase):
     def setUp(self):
         self.obj = createObj("Infineon")
 
-    '''
+
     def test_ReadID(self):
         id = self.obj.getID()
         self.assertEqual(id, hex(0x10))
@@ -38,62 +38,94 @@ class InfineonTests(unittest.TestCase):
     def test_stand_by(self):
         meas_config_address = self.obj.getAddress('meas_config')
         #test standby
-        self.obj.setState('stand-by')
+
+        key = 'stand-by'
+        self.obj.setState(key)
 
         actual = bin(self.obj.readAddress(meas_config_address))
         actual = int(actual[7:], 2)
-        self.assertEqual(actual, 0b000)
+
+        expected = self.obj.states[key]
+
+        self.assertEqual(actual, expected)
 
     def test_temp_meas(self):
         #test temp measurement
         meas_config_address = self.obj.getAddress('meas_config')
-        self.obj.setState('temp_meas')
+
+        key = 'stand-by'
+        self.obj.setState(key)
+
+        key = 'temp_meas'
+        self.obj.setState(key)
+
         actual = bin(self.obj.readAddress(meas_config_address))
 
+        expected = self.obj.states[key]
         actual = int(actual[7:], 2)
-        self.assertEqual(actual, 0b010)
+        self.assertEqual(actual, expected)
 
     def test_press_meas(self):
         # test temp measurement
         meas_config_address = self.obj.getAddress('meas_config')
-        self.obj.setState('press_meas')
+
+        key = 'stand-by'
+        self.obj.setState(key)
+
+        key = "press_meas"
+        self.obj.setState(key)
         actual = bin(self.obj.readAddress(meas_config_address))
         actual = int(actual[7:], 2)
 
-        self.assertEqual(actual, 0b001)
-        '''
+        expected = self.obj.states[key]
+        self.assertEqual(actual, expected)
+
     def test_press_cont(self):
         # test press measurement
         meas_config_address = self.obj.getAddress('meas_config')
+
+        key = 'stand-by'
+        self.obj.setState(key)
+
         key = 'cont_press'
         self.obj.setState(key)
         actual = bin(self.obj.readAddress(meas_config_address))
-        print(actual)
-        print(actual[7:])
+
         actual = int(actual[7:], 2)
-        print(actual)
         expected = self.obj.states[key]
-        print(expected)
+
         self.assertEqual(actual, expected)
-'''
+
     def test_temp_cont(self):
         # test temp measurement
         meas_config_address = self.obj.getAddress('meas_config')
-        self.obj.setState('cont_temp')
+
+        key = 'stand-by'
+        self.obj.setState(key)
+
+        key = 'cont_temp'
+        self.obj.setState(key)
         actual = bin(self.obj.readAddress(meas_config_address))
         actual = int(actual[7:], 2)
 
-        self.assertEqual(actual, 0b110)
+        expected = self.obj.states[key]
+        self.assertEqual(actual, expected)
 
     def test_both_cont(self):
         # test temp measurement
         meas_config_address = self.obj.getAddress('meas_config')
-        self.obj.setState('cont_both')
+
+        key = 'stand-by'
+        self.obj.setState(key)
+
+        key = 'cont_both'
+        self.obj.setState(key)
         actual = bin(self.obj.readAddress(meas_config_address))
         actual = int(actual[7:], 2)
 
-        self.assertEqual(actual, 0b111)
-'''
+        expected = self.obj.states[key]
+        self.assertEqual(actual, expected)
+
 if __name__ == '__main__':
     unittest.main()
 
